7e7e01be0c8479ecec071906cee92ce67dd3d2ca
Kate <skipperiniap@gmail.com>
2020-11-08 20:50:28 +0300

    add LR05_OP

---
+++ Lr05_OP.cpp
@@ -0,0 +1,44 @@
+// Lr05_OP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+#include "windows.h"
+using namespace std;
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int x, a, b, c;
+	double f;
+	cout << "¬вед≥ть x: ";
+	cin >> x;
+	cout << "¬вед≥ть a: ";
+	cin >> a;
+	cout << "¬вед≥ть b: ";
+	cin >> b;
+	cout << "¬вед≥ть c: ";
+	cin >> c;
+
+	if ((x + c) < 0 && a != 0) { f = -a * pow(x, 3) - b; }
+	if ((x + c) > 0 && a == 0) { f = (x - a) / (x - c); }
+	else { f = (x / c) + (c / x); }
+	cout << "F=" << f << endl;
+	system("pause");
+	return 0;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
